4a8beae7c768ce2d1c84b0d992713a6d7c83fd81
samade747 <samad.e747@gmail.com>
2023-12-25 13:07:14 +0500

    1:06pm 25 12 2023 news api image & search the news br search function

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
+let weatherData = document.querySelector('#weatherData');
+
 let country = 'us';
 let API_KEY = '46485d2fcdf2462db53b3976110afc57';
 fetch(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${API_KEY}`)
 .then(data => data.json())
-.then(data => console.log(data))
-.catch(err => console.log(err))
+.then(data => {
+    for(let i = 0; i < data.articles.length; i++){
+        weatherData.innerHTML += `
+
+        <div class="card m-2" style="width: 18rem;">
+  <img src="${data.articles[i].urlToImage}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+
+        `
+        // console.log(data.articles[i])
+    }
+});
+
+
+
+
+ 
+// .catch(err => console.log(err))
